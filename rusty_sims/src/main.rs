0a0b67ac50b1812a8952f4fce3074b32622e4799
HamzaMateen <hamzamateen1583@gmail.com>
2024-03-04 23:51:27 +0500

    WIP adding tui to the application (2)

--- rusty_sims/src/main.rs
+++ rusty_sims/src/main.rs
@@ -1,28 +1,26 @@
-use std::io;
+use tui_textarea::TextArea;
+
+// Ratatui from here
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    layout::Alignment,
     prelude::*,
     symbols::border,
     widgets::{block::*, *},
 };
 
 mod tui;
+use std::io;
 
-// end using tui stuff
-mod auth;
-mod database;
-mod inventory;
-mod misc;
+fn main() -> io::Result<()> {
+    let mut terminal = tui::init()?;
 
-use rusqlite::Connection;
+    let app_result = App::default().run(&mut terminal);
 
-use crate::{
-    database::create_tables,
-    inventory::{add_product, edit_product, remove_product},
-    misc::strip_right,
-};
+    tui::restore()?;
+
+    app_result
+}
 
 #[derive(Debug, Default)]
 pub struct App {
@@ -31,20 +29,35 @@ pub struct App {
 }
 
 impl App {
-    // runs the app main loop the user quits
+    /// runs the application's main loop until the user quits
     pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
         }
         Ok(())
     }
 
     fn render_frame(&self, frame: &mut Frame) {
+        let rect = ratatui::layout::Rect {
+            x: 5,
+            y: 5,
+            width: 20,
+            height: 20,
+        };
+
+        let textarea = TextArea::default();
+        let textarea_widget = textarea.widget();
+
+        frame.render_widget(textarea_widget, rect);
         frame.render_widget(self, frame.size());
     }
 
+    /// updates the application's state based on user input
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
@@ -56,38 +69,37 @@ impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.increment_counter(),
-            KeyCode::Right => self.decrement_counter(),
+            KeyCode::Char('j') => self.decrement_counter(),
+            KeyCode::Char('k') => self.increment_counter(),
             _ => {}
         }
     }
 
-    // business logic
     fn exit(&mut self) {
         self.exit = true;
     }
 
-    fn decrement_counter(&mut self) {
+    fn increment_counter(&mut self) {
         self.counter += 1;
     }
 
-    fn increment_counter(&mut self) {
+    fn decrement_counter(&mut self) {
         self.counter -= 1;
     }
 }
 
+// ANCHOR: impl Widget
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Welcome to RustySIMS ".bold());
+        let title = Title::from(" Counter App Tutorial ".bold());
         let instructions = Title::from(Line::from(vec![
             " Decrement ".into(),
             "<Left>".blue().bold(),
             " Increment ".into(),
             "<Right>".blue().bold(),
-            " Quit ".bold(),
-            " Q ".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
         ]));
-
         let block = Block::default()
             .title(title.alignment(Alignment::Center))
             .title(
@@ -106,96 +118,6 @@ impl Widget for &App {
         Paragraph::new(counter_text)
             .centered()
             .block(block)
-            .render(area, buf)
+            .render(area, buf);
     }
-}
-
-fn main() -> io::Result<()> {
-    let mut terminal = tui::init()?;
-
-    let mut myapp = App::default();
-    myapp.run(&mut terminal)?;
-
-    tui::restore()?;
-    Ok(())
-
-    /*
-
-    // 1. Print the welcome screen
-    //
-    let title = "Welcome to RustySIMS";
-    let sub_title = "You one-stop Inventory Management Solution.";
-
-    misc::print_welcome_msg(title, sub_title, 60);
-
-    Ok(())
-    // 2. Authentication
-    println!("\n\nDo you want to register or login to the platform?");
-    println!("Enter 1 to register yourself");
-    println!("Enter 2 to login to the platform");
-
-    let mut login_choice_str: String = String::from("");
-    stdin()
-        .read_line(&mut login_choice_str)
-        .expect("Error reading manager's choice");
-    strip_right(&mut login_choice_str);
-
-    println!("\nOk ...");
-    let mut name: String = "".to_string();
-    let mut password: String = "".to_string();
-
-    print!("\nEnter your name: ");
-    io::stdout().flush().expect("Error flushing stdout");
-
-    stdin()
-        .read_line(&mut name)
-        .expect("Error reading the name from terminal");
-    strip_right(&mut name);
-
-    print!("Enter your password: ");
-    io::stdout().flush().expect("Error flushing stdout");
-
-    stdin()
-        .read_line(&mut password)
-        .expect("Error reading the name from terminal");
-    strip_right(&mut password);
-
-    // create connection to the database
-    let conn: Connection =
-        Connection::open("./database.db3").expect("Error connecting to the database");
-    create_tables(&conn).expect("Error creating the tables");
-
-    match login_choice_str.as_str() {
-        "1" => {
-            match auth::create_manager(&conn, name.as_str(), password.as_str()) {
-                Ok(_) => {
-                    println!("Manager created succesfully!");
-                    ()
-                }
-                Err(e) => println!("{}", e),
-            };
-        }
-        "2" => {
-            auth::authenticate_manager(&conn, name.as_str(), password.as_str());
-            println!("Manager authenticated succesfully");
-            ()
-        }
-        _ => {
-            println!("\nInvalid choice, please reconsider. Aborting ... \n");
-            std::process::abort();
-        }
-    }
-
-    // 3. add a product to the inventory
-    // let's do it
-    // add_product(&conn, name.as_str());
-
-    // 3.5 edit a product
-    // at this time, we have the manager's name
-    let product_name = "Love";
-    // edit_product(&conn, product_name, "hamza");
-
-    // 4. Remove a product from the inventory
-    remove_product(&conn, name.as_str());
-    */
 }
--- rusty_sims/src/tui.rs
+++ rusty_sims/src/tui.rs
@@ -1,20 +1,29 @@
-use crossterm::{execute, terminal::*};
-use ratatui::prelude::*;
+// ANCHOR: all
+// ANCHOR: imports
 use std::io::{self, stdout, Stdout};
 
+use crossterm::{execute, terminal::*};
+use ratatui::prelude::*;
+// ANCHOR_END: imports
+
+// ANCHOR: init
 /// A type alias for the terminal type used in this application
 pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
-// Initialize the terminal
+/// Initialize the terminal
 pub fn init() -> io::Result<Tui> {
-    execute!(stdout(), EnterAlternateScreen);
-    enable_raw_mode()?;
+    execute!(stdout(), EnterAlternateScreen)?;
+    // enable_raw_mode()?;
     Terminal::new(CrosstermBackend::new(stdout()))
 }
+// ANCHOR_END: init
 
-// Restore the terminal
+// ANCHOR: restore
+/// Restore the terminal to its original state
 pub fn restore() -> io::Result<()> {
     execute!(stdout(), LeaveAlternateScreen)?;
-    disable_raw_mode()?;
+    // disable_raw_mode()?;
     Ok(())
 }
+// ANCHOR_END: restore
+// ANCHOR_END: all
